06c8f1445ff6bd3c181efc263b48db79371d617c
keyuhan <hankeyu2021@gmail.com>
2023-02-07 21:59:36 -0600

    20230206 course

---
+++ src/main/java/com/cogent/basics/Calculator.java
@@ -0,0 +1,14 @@
+package com.cogent.basics;
+
+public class Calculator {
+	
+	static int no1 = 1;
+	static int no2 = 2;
+	static int no3 = 3;
+	
+	
+	public static void main(String[] args) {
+		int res = no1 + no2 + no3;
+		System.out.println(res);
+	}
+}
---
+++ src/main/java/com/cogent/basics/info.java
@@ -0,0 +1,10 @@
+package com.cogent.basics;
+
+public class info {
+	
+	public static void main(String [] args) {
+		System.out.print("Keyu Han ");
+		System.out.println("loves Java");
+
+	}
+}
